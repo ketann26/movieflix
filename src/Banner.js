70dc0931dbec868431086bda4a4c87a9aa26d009
ketann26 <ketan9sharma@yahoo.in>
2024-01-11 14:26:22 +0530

    Initital Commit

---
+++ src/Banner.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Banner.css";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  const fetchData = async () => {
+    const request = await axios.get(requests.fetchNetflixOriginals);
+    setMovie(
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+      ]
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner-content">
+        <h1 className="banner-title">{movie?.name}</h1>
+        <div className="banner-buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
+        </div>
+        <h1 className="banner-desc">{movie?.overview}</h1>
+      </div>
+      <div className="banner-bottom"></div>
+    </header>
+  );
+};
+
+export default Banner;
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.png";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav-bg"}`}>
+      <img src={logo} alt="Logo" classname="nav-logo" height="50px" />
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+        alt=""
+        className="nav-avatar"
+        height="50px"
+      />
+    </div>
+  );
+};
+
+export default Nav;
