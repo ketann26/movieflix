d27760be1f89ef87902a0f0a31289f975532fef8
ketann26 <ketan9sharma@yahoo.in>
2024-01-11 15:03:58 +0530

    Re-deploy on Firebase

--- src/Row.js
+++ src/Row.js
@@ -5,35 +5,35 @@ import "./Row.css";
 const poster_base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchURL, isLargeRow }) => {
-  const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
 
-  const fetchData = async () => {
-    const request = await axios.get(fetchURL);
-    setMovies(request.data.results);
-    return request;
-  };
+	const fetchData = async () => {
+		const request = await axios.get(fetchURL);
+		setMovies(request.data.results);
+		return request;
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchURL]);
+	useEffect(() => {
+		fetchData();
+	});
 
-  return (
-    <div className="row">
-      <h2>{title}</h2>
-      <div className="row-item-container">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={`row-item ${isLargeRow && "poster-lg"}`}
-            src={`${poster_base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="row">
+			<h2>{title}</h2>
+			<div className="row-item-container">
+				{movies.map((movie) => (
+					<img
+						key={movie.id}
+						className={`row-item ${isLargeRow && "poster-lg"}`}
+						src={`${poster_base_url}${
+							isLargeRow ? movie.poster_path : movie.backdrop_path
+						}`}
+						alt={movie.name}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Row;
