3b4d87c68db8dd687093c27083a838d656a5148d
ketann26 <ketan9sharma@yahoo.in>
2024-01-11 15:23:33 +0530

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -4,43 +4,39 @@ import requests from "./requests";
 import "./Banner.css";
 
 const Banner = () => {
-  const [movie, setMovie] = useState([]);
+	const [movie, setMovie] = useState([]);
 
-  const fetchData = async () => {
-    const request = await axios.get(requests.fetchNetflixOriginals);
-    setMovie(
-      request.data.results[
-        Math.floor(Math.random() * request.data.results.length - 1)
-      ]
-    );
-  };
+	const fetchData = async () => {
+		const request = await axios.get(requests.fetchNetflixOriginals);
+		setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+	};
 
-  useEffect(() => {
-    fetchData();
-  });
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  console.log(movie);
+	console.log(movie);
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
-      }}
-    >
-      <div className="banner-content">
-        <h1 className="banner-title">{movie?.name}</h1>
-        <div className="banner-buttons">
-          <button className="button">Play</button>
-          <button className="button">My List</button>
-        </div>
-        <h1 className="banner-desc">{movie?.overview}</h1>
-      </div>
-      <div className="banner-bottom"></div>
-    </header>
-  );
+	return (
+		<header
+			className="banner"
+			style={{
+				backgroundSize: "cover",
+				backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+				backgroundPosition: "center center",
+			}}
+		>
+			<div className="banner-content">
+				<h1 className="banner-title">{movie?.name}</h1>
+				<div className="banner-buttons">
+					<button className="button">Play</button>
+					<button className="button">My List</button>
+				</div>
+				<h1 className="banner-desc">{movie?.overview}</h1>
+			</div>
+			<div className="banner-bottom"></div>
+		</header>
+	);
 };
 
 export default Banner;
